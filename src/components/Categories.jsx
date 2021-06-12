f70cec5279c03b5fbce06cafb117beb5dd65d82c
Illia <ilya.kvitkovskyi@gmail.com>
2021-06-12 17:15:13 +0300

    added useState and refactor Categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-function Categories() {
+function Categories({ items, onClickItem }) {
+  const [activeItem, setActiveItem] = React.useState(null);
+
+  const onSelectItem = (index) => {
+    setActiveItem(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        <li className={activeItem === null ? 'active' : ''} onClick={() => onSelectItem(null)}>
+          Все
+        </li>
+        {items &&
+          items.map((name, index) => (
+            <li
+              className={activeItem === index ? 'active' : ''}
+              onClick={() => onSelectItem(index)}
+              key={`${name}_${index}`}>
+              {name}
+            </li>
+          ))}
       </ul>
     </div>
-  )
+  );
 }
 
 export default Categories;
