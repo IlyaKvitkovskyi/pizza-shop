bf2062d52ad986483b627a5e12c81cceeea50e97
Illia <ilya.kvitkovskyi@gmail.com>
2021-06-18 13:35:50 +0300

    added redux library

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createStore } from 'redux';
 
 import './scss/app.scss';
 
 import App from './App';
+
+function counter(state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counter);
 
 ReactDOM.render(
   <React.StrictMode>
