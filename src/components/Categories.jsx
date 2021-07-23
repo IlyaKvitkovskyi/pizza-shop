0b4535714e383c00015b47ada77fcc714b112cd7
Illia <ilya.kvitkovskyi@gmail.com>
2021-07-23 18:32:29 +0300

    removed onSelectItem and added redux action

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Categories = React.memo(function Categories({ activeCategory, items, onClickItem }) {
-  const onSelectItem = (index) => {
-    onClickItem(index);
-  };
-
+const Categories = React.memo(function Categories({ activeCategory, items, onClickCategory }) {
   return (
     <div className="categories">
       <ul>
-        <li className={activeCategory === null ? 'active' : ''} onClick={() => onSelectItem(null)}>
+        <li className={activeCategory === null ? 'active' : ''} onClick={() => onClickCategory(null)}>
           Все
         </li>
         {items &&
           items.map((name, index) => (
             <li
               className={activeCategory === index ? 'active' : ''}
-              onClick={() => onSelectItem(index)}
+              onClick={() => onClickCategory(index)}
               key={`${name}_${index}`}>
               {name}
             </li>
@@ -24,5 +21,13 @@ const Categories = React.memo(function Categories({ activeCategory, items, onCli
     </div>
   );
 });
+
+Categories.propTypes = {
+  activeCategory: PropTypes.number.isRequired,
+  items: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onClickCategory: PropTypes.func,
+};
+
+Categories.defaultProps = { activeCategory: null, items: [] };
 
 export default Categories;
